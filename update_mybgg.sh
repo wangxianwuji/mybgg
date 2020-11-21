8cc983964764995bf13dac65fcf8eec932a21053
boglesby03 <boglesby+github@gmail.com>
2020-11-20 20:06:42 -0600

    Delete update_mybgg.sh

--- update_mybgg.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-#python scripts/download_and_index.py --apikey 538f75c7daa2741396133f69c981b172 --no_indexing --cache_bgg
-python scripts/download_and_index.py --apikey 538f75c7daa2741396133f69c981b172
